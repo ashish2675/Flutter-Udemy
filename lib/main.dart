49ab813e22a42ca8cad5ac2bec33a08c1e61593f
Ashish Jose <ashishjose2675@gmail.com>
2022-08-09 22:20:28 +0530

    mod 54 completed

--- lib/main.dart
+++ lib/main.dart
@@ -18,24 +18,44 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final _questions = [
-    {
-      'questionText': 'Whats\'s your favorite color?',
-      'answers': ['Red', 'Blue', 'Orange', 'Purple'],
-    },
-    {
-      'questionText': 'What\'s your favorite animal?',
-      'answers': ['Bear', 'Raindear', 'Horse', 'Cat'],
-    },
-    {
-      'questionText': 'Who\'s your favorite instructor?',
-      'answers': ['Akhand', 'Chaman', 'Suman', 'Aman'],
-    },
+    [
+      {
+        'questionText': 'What\'s your favorite color?',
+        'answers': [
+          {'text': 'Black', 'score': 10},
+          {'text': 'Red', 'score': 5},
+          {'text': 'Green', 'score': 3},
+          {'text': 'White', 'score': 1},
+        ],
+      },
+      {
+        'questionText': 'What\'s your favorite animal?',
+        'answers': [
+          {'text': 'Rabbit', 'score': 3},
+          {'text': 'Snake', 'score': 11},
+          {'text': 'Elephant', 'score': 5},
+          {'text': 'Lion', 'score': 9},
+        ],
+      },
+      {
+        'questionText': 'Who\'s your favorite instructor?',
+        'answers': [
+          {'text': 'Max', 'score': 1},
+          {'text': 'Max', 'score': 1},
+          {'text': 'Max', 'score': 1},
+          {'text': 'Max', 'score': 1},
+        ],
+      },
+    ],
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
     // var aBool = true;
     // aBool = false;
+
+    _totalScore += score;
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -62,7 +82,7 @@ class _MyAppState extends State<MyApp> {
               questionIndex: _questionIndex,
               questions: _questions,
             )
-          : Result(),
+          : Result(_totalScore),
     ));
   }
 }
