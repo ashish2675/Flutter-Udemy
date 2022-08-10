f74896b2c649e13e2300e09ef0ef8a65d9207b62
Ashish Jose <ashishjose2675@gmail.com>
2022-08-10 23:49:26 +0530

    mod 55 completed

--- lib/main.dart
+++ lib/main.dart
@@ -18,38 +18,43 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final _questions = [
-    [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': [
-          {'text': 'Black', 'score': 10},
-          {'text': 'Red', 'score': 5},
-          {'text': 'Green', 'score': 3},
-          {'text': 'White', 'score': 1},
-        ],
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': [
-          {'text': 'Rabbit', 'score': 3},
-          {'text': 'Snake', 'score': 11},
-          {'text': 'Elephant', 'score': 5},
-          {'text': 'Lion', 'score': 9},
-        ],
-      },
-      {
-        'questionText': 'Who\'s your favorite instructor?',
-        'answers': [
-          {'text': 'Max', 'score': 1},
-          {'text': 'Max', 'score': 1},
-          {'text': 'Max', 'score': 1},
-          {'text': 'Max', 'score': 1},
-        ],
-      },
-    ],
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+      ],
+    },
   ];
   var _questionIndex = 0;
   var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
 
   void _answerQuestion(int score) {
     // var aBool = true;
@@ -82,7 +87,7 @@ class _MyAppState extends State<MyApp> {
               questionIndex: _questionIndex,
               questions: _questions,
             )
-          : Result(_totalScore),
+          : Result(_totalScore, _resetQuiz),
     ));
   }
 }
